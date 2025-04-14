86383ef1b49c94616e8061bd68135b03a1179013
sidratello <sidratello2@gmail.com>
2025-04-14 12:40:36 +0300

    i edit the main file

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-   initialRoute: "/home3",
+   initialRoute: "/home",
         getPages: [
         
         GetPage(name: "/onbording", page: () => onbording(),),
