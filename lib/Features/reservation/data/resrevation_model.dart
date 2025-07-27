d2a4657d74aff9e5118fee4f43ef54ae5e2834ea
Sham_Hpopaty <shamhpopaty@gmail.com>
2025-07-27 10:15:43 +0300

    last edit 2

---
+++ lib/Features/reservation/data/resrevation_model.dart
@@ -0,0 +1,15 @@
+class ReservationModelForm {
+  String? date;
+  String? guestsCount;
+  String? notes;
+  String? startTime;
+  String? endTime;
+
+  ReservationModelForm({
+    required this.date,
+    required this.guestsCount,
+    required this.notes,
+    required this.startTime,
+    required this.endTime,
+  });
+}
